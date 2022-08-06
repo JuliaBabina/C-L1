ad7aa5d7ae23ccf3ab4a0f29837f22b8c5d6e33f
Julia Babina <u.babina55@gmail.com>
2022-08-06 19:06:02 +0500

    add Example010 Find

---
+++ Example010_Find/Program.cs
@@ -0,0 +1,19 @@
+﻿// Имеется одномерный массив array их n злементов, требуется нв ти элемент массива, равный find
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
