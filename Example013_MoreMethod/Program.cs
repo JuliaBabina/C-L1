7209c07ad7b1d02b8f843b252f54afb81c0526a5
Julia Babina <u.babina55@gmail.com>
2022-08-13 15:32:44 +0500

    add Ex3 More Method

---
+++ Example013_MoreMethod/Program.cs
@@ -0,0 +1,221 @@
+﻿// Виды методов
+
+// 1 вид. Ничего не принимает, ничего не возвращает (напрмер прописывать авторство)
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+
+// // Как вызываются:
+
+// Method1();
+
+
+// 2 вид. ничего не возвращают, но могут принимать аргументы
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// // Method2("текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+
+// // Method21(msg: "текст", count: 4);
+// Method21(count: 4, msg: "Новый текст");
+
+// 3 вид. Что-то возвращают, но ничего не принимают
+
+// int Method3() // тип данный обязательно, т.к. возвращает
+// {
+// return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// 4 вид. Что-то принимают, что-то возвращают
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+
+//     while (i<count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// *************************** цикл for**********************
+
+// string Method4(int count, string text)
+// {
+//     string result = string.Empty;
+//     for (int i = 0; i < count; i++)
+
+//         {
+//             result = result + text;
+
+//         }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// **********************************цикл внутри цикла*******************************
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i}x{j} = {i * j}");
+//     }
+
+//     Console.WriteLine();
+// }
+
+//***********************задачи*****************************************************
+
+// Работа с текстом 
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = 'qwerty'
+//             012345
+// s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+
+//     return result;
+// }
+
+// string newText  = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+
+//
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+
+//     return result;
+// }
+
+// string newText  = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText  = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText  = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+//*******************************упорядочивание массивов********************************
+
+// Метод минмального максимального
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrinrArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+
+    }
+}
+
+void SelectionSort2(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+
+
+    }
+}
+
+
+PrinrArray(arr);
+
+SelectionSort(arr);
+PrinrArray(arr);
+
+SelectionSort2(arr);
+PrinrArray(arr);
+
